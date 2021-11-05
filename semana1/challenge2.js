6301d3891372acbeec78ef3a8fa444c3877d443f
David Valero <dvdovni@gmail.com>
2021-11-04 19:20:02 -0500

    Ejercicios semana 1 ok

--- semana1/challenge2.js
+++ semana1/challenge2.js
@@ -1,7 +1,27 @@
+///Ejercicio no.2
+// ✕ Debería de invertir los dígitos del entero 123 a 321 (33 ms)
+// ✕ Debería de invertir los dígitos del entero 13689 a 98631 (10 ms)
+// ✕ Debería de invertir los dígitos del entero 13940 a 4931 (13 ms)
+// ✕ Debería de invertir los dígitos del entero -13940 a -4931 (15 ms)
+// ✕ Debería de invertir los dígitos del entero -13940 a -4931 (13 ms)
+// ✕ Debería de arrojar un error al pasar un tipo de dato diferente a number como argumento (1 ms)
+
 const reverseInt = (number) => {
-    //Implementación
-}
+  //Implementación
+  if (typeof number === 'number') {
+    let negative = false;
+    if (number < 0) {
+      negative = true;
+    }
+    let num = String(number);
+    num = num.split('').reverse().join('');
+    num = parseInt(num);
+    negative ? (num = num * -1) : num;
+    return num;
+  }
+  throw new Error('Argumento no valido');
+};
 
 module.exports = {
-    reverseInt
-}
+  reverseInt,
+};
--- semana1/challenge3.js
+++ semana1/challenge3.js
@@ -1,7 +1,13 @@
 const capitalizeLetters = (str) => {
-    //Implementación
-}
+  //Implementación
+  const arrayWords = str.split(' ');
+  let arrayWordsCapitalized = arrayWords.map(
+    (word) => word[0].toUpperCase() + word.slice(1)
+  );
+  arrayWordsCapitalized = arrayWordsCapitalized.join(' ');
+  return arrayWordsCapitalized;
+};
 
 module.exports = {
-    capitalizeLetters
-}
+  capitalizeLetters,
+};
